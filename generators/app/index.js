b6035096223e8e618fc3431075e3035229801b2c
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-02-10 21:39:20 -0200

    add: condition for task runner

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,7 +5,7 @@ var yosay = require('yosay');
 var mkdirp = require('mkdirp');
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function() {
+  askFor: function() {
     var done = this.async();
 
     var prompts = [{
@@ -13,12 +13,33 @@ module.exports = yeoman.generators.Base.extend({
       name: 'name',
       message: 'Your project name',
       default: this.appname
+    },
+    {
+      type: 'list',
+      name: 'taskRunner',
+      message: 'Which task runner would you like to use?',
+      choices: [{
+        name: 'GruntJS',
+        value: 'includeGrunt'
+      },{
+        name: 'GulpJS',
+        value: 'includeGulp'
+      },{
+        name: 'None',
+        value: ''
+      }]
     }];
 
     this.prompt(prompts, function(answers) {
+      var features = answers.features;
 
       this.props = answers;
-      this.log(answers.name);
+
+      function hasFeature(feat) {
+        return features && features.indexOf(feat) !== -1;
+      }
+
+      this.taskRunner = answers.taskRunner;
 
       done();
     }.bind(this));
@@ -26,13 +47,37 @@ module.exports = yeoman.generators.Base.extend({
   },
 
   writing: {
-    config: function() {
+    editorConfig: function () {
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+    },
+
+    git: function () {
+      this.fs.copy(
+        this.templatePath('gitignore'),
+        this.destinationPath('.gitignore')
+      );
+
+      this.fs.copy(
+        this.templatePath('gitattributes'),
+        this.destinationPath('.gitattributes')
+      );
+    },
+
+    packageJSON: function () {
       this.fs.copyTpl(
         this.templatePath('_package.json'),
-        this.destinationPath('package.json'), {
+        this.destinationPath('package.json'),
+        {
+          taskRunner: this.taskRunner,
           name: this.props.name
         }
-      );
+      )
+    },
+
+    config: function() {
       this.fs.copyTpl(
         this.templatePath('_bower.json'),
         this.destinationPath('bower.json'), {
@@ -46,9 +91,37 @@ module.exports = yeoman.generators.Base.extend({
     },
 
     misc: function () {
-      mkdirp('assets/css');
-      mkdirp('assets/js');
-      mkdirp('assets/img');
+      mkdirp('source');
+      mkdirp('source/assets');
+    },
+
+    taskRunner: function () {
+      mkdirp('source/tasks');
+
+      switch(this.taskRunner) {
+        case 'includeGrunt':
+          mkdirp('source/tasks/gruntjs');
+          this.fs.copy(
+            this.templatePath('Gruntfile.js'),
+            this.destinationPath('Gruntfile.js')
+          );
+          this.fs.copy(
+            this.templatePath('gruntjs/aliases.yaml'),
+            this.destinationPath('source/tasks/gruntjs/aliases.yaml')
+          );
+          break;
+        case 'includeGulp':
+          mkdirp('source/tasks/gulpjs');
+          this.fs.copy(
+            this.templatePath('Gulpfile.js'),
+            this.destinationPath('Gulpfile.js')
+          );
+          this.fs.copy(
+            this.templatePath('gulpjs/default.js'),
+            this.destinationPath('source/tasks/gulpjs/default.js')
+          );
+          break;
+      }
     }
   },
   install: function() {
